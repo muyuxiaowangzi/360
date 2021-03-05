7e51773c99f1c866b27328234f850108ea038570
stranger <599040290@qq.com>
2021-03-05 16:53:03 +0800

    day02

---
+++ rmy/360shop/src/plugins/swiper.js
@@ -0,0 +1,8 @@
+// 引入VUe
+import Vue from 'vue'
+// 引入vue-awesome-swiper
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+// 引入css文件
+import 'swiper/css/swiper.css'
+// 声明使用插件
+Vue.use(VueAwesomeSwiper)
---
+++ rmy/360shop/src/views/shop/ImageList/index.vue
@@ -0,0 +1,93 @@
+<template>
+    <!-- 轮播图 -->
+        <!-- <div class="swiper">
+          <div class="btn">
+            <button class="leftBtn">&lt;</button>
+            <button class="rightBtn">&gt;</button>
+          </div>
+          <div class="list">
+            <ul>
+              <li class="imgItem">
+                <div class="img">
+                  <img src="../../static/images/2.png" alt />
+                </div>
+              </li>
+              <li class="imgItem">
+                <div class="img">
+                  <img src="../../static/images/3.png" alt />
+                </div>
+              </li>
+              <li class="imgItem">
+                <div class="img">
+                  <img src="../../static/images/4.png" alt />
+                </div>
+              </li>
+              <li class="imgItem">
+                <div class="img">
+                  <img src="../../static/images/5.png" alt />
+                </div>
+              </li>
+              <li class="imgItem">
+                <div class="img">
+                  <img src="../../static/images/6.png" alt />
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div> -->
+        <swiper class="swiper" :options="{
+                      slidesPerView:5, // 显示几个图片(每次限时几屏)
+                      slidesPerGroup:1, // 每次移动几个图片 
+                    // 如果需要前进后退按钮
+                    navigation: {
+                    nextEl: '.swiper-button-next',
+                      prevEl: '.swiper-button-prev',
+                    }, 
+            }"
+          >
+                <swiper-slide class="swiper-slide">
+                    <img src="../../../static/images/2.png" />
+                </swiper-slide>
+                <swiper-slide class="swiper-slide">
+                    <img src="../../../static/images/3.png" />
+                </swiper-slide>
+                <swiper-slide class="swiper-slide">
+                    <img src="../../../static/images/4.png" />
+                </swiper-slide>
+                <swiper-slide class="swiper-slide">
+                    <img src="../../../static/images/5.png" />
+                </swiper-slide>
+                <swiper-slide class="swiper-slide">
+                    <img src="../../../static/images/6.png" />
+                </swiper-slide>
+                <swiper-slide class="swiper-slide">
+                    <img src="../../../static/images/5.png" />
+                </swiper-slide>
+                <swiper-slide class="swiper-slide">
+                    <img src="../../../static/images/6.png" />
+                </swiper-slide>
+                <swiper-slide class="swiper-slide">
+                    <img src="../../../static/images/5.png" />
+                </swiper-slide>
+                <swiper-slide class="swiper-slide">
+                    <img src="../../../static/images/6.png" />
+                </swiper-slide>
+                    <div class="swiper-button-prev" slot="button-prev"></div>
+                    <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
+</template>
+<script>
+export default {
+    name:'ImageList'
+}
+</script>
+<style lang="less" scoped>
+      .swiper{
+        .swiper-slide{
+          img{
+             height: 88px;
+             width: 88px;
+          }
+        }
+      }
+</style>
